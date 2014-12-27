17debca461f8e5910e68a6dcb4b47f816c06f380
OHASHI Hideya <ohachige@gmail.com>
2014-12-27 19:58:58 +0900

    use local storage

--- public/index.js
+++ public/index.js
@@ -1,4 +1,21 @@
 $(function(){
-  localStorage.setItem('foo', 'bar');
-  var foo = localStorage.getItem('foo');
+  var encdigest = $('#encdigest').val();
+  if (encdigest) {
+    var name = $('#name').val();
+    var digest = $('#digest').val();
+    localStorage.setItem(encdigest + ':name', name);
+    localStorage.setItem(encdigest + ':digest', digest);
+  }
+  $('.files').each(function(i, x) {
+    var y = $(x);
+    var id = y.attr('id');
+    var digest = localStorage.getItem(id + ':digest');
+    if (digest) {
+      var name = localStorage.getItem(id + ':name');
+      var href = y.attr('href');
+      href = href + '?digest=' + digest + '&filename=' + name;
+      y.attr('href', href);
+      y.html(name);
+    }
+  });
 });
