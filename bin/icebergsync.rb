f46f3fdddb98815d732157e06453e10e7b559d54
OHASHI Hideya <ohachige@gmail.com>
2015-01-02 22:32:27 +0900

    upload files

--- bin/icebergsync.rb
+++ bin/icebergsync.rb
@@ -14,6 +14,38 @@ require 'net/http'
 require 'json'
 require 'fileutils'
 
+class MultiReader
+
+  def initialize
+    @readers = []
+    @cur = 0
+  end
+
+  def add(reader)
+    @readers << reader
+  end
+
+  def read(size = nil)
+    rv = ''
+    loop do
+      reader = @readers[@cur]
+      break unless reader
+      s = reader.read(size)
+      if s
+        rv += s
+        if size
+          size -= s.size
+          break if size <= 0
+        end
+      else
+        @cur += 1
+      end
+    end
+    rv.size > 0 ? rv : nil
+  end
+
+end
+
 class FileHub
 
   def initialize(uri)
@@ -47,22 +79,30 @@ class FileHub
   end
 
   def uploadraw(path)
-    Net::HTTP.start(@host, @port) do |http|
-      token = gettoken(http)
-      encdigest = 'ffff' # TODO
-      req = Net::HTTP::Post.new('/api/v1/uploadraw/' + encdigest)
-      boundary = 'myboundary' # TODO
-      data = 'foobar' # TODO
+    Net::HTTP.start(@uri.host, @uri.port) do |http|
+      encdigest = File.basename(path)
+      req = Net::HTTP::Post.new('/api/v1/uploadraw')
+      boundary = (0...50).map { (65 + rand(26)).chr }.join
       req.set_content_type("multipart/form-data; boundary=#{boundary}")
-      req.body = <<EOF
+      body1 = StringIO.new(<<EOF)
 --#{boundary}\r
-Content-Disposition: form-data; name="#{encdigest}"\r
+Content-Disposition: form-data; name="file"; filename="#{encdigest}"\r
 Content-Type: application/octet-stream\r
 Content-Transfer-Encoding: binary\r
 \r
-#{data}\r
---#{boundary}--\r
 EOF
+      body2 = File.open(path, 'rb')
+      body3 = StringIO.new(<<EOF)
+--#{boundary}--\r
+\r
+
+EOF
+      mr = MultiReader.new
+      mr.add(body1)
+      mr.add(body2)
+      mr.add(body3)
+      req.body_stream = mr
+      req.content_length = body1.size + File.size(path) + body3.size
       res = http.request(req)
       body = res.body
       JSON.parse(body)
@@ -74,9 +114,18 @@ end
 download = SETTING['local']['download']
 hub = FileHub.new('http://box.sighash.info') # TODO
 json = hub.recentfiles()
-json['recentfiles'].each do |encdigest|
+recentfiles = json['recentfiles']
+recentfiles.each do |encdigest|
   file = File.join(download, encdigest)
   next if File.exist?(file)
   puts "download: #{encdigest}"
   hub.download(encdigest, file)
+end
+dir = Dir.new(download)
+dir.each do |file|
+  next unless file.size == 40
+  unless recentfiles.index(file)
+    puts "upload: #{file}"
+    json = hub.uploadraw(File.join(download, file))
+  end
 end
