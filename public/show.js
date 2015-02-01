c1b97cf291401197cc5ce1c15eae73154c068632
OHASHI Hideya <ohachige@gmail.com>
2015-02-01 22:31:50 +0900

    import keys button

--- public/show.js
+++ public/show.js
@@ -6,4 +6,24 @@ $(function(){
     localStorage.setItem(encdigest + ':name', name);
     localStorage.setItem(encdigest + ':digest', digest);
   }
+  $('#apply').click(function () {
+    var textdata = $('#textdata');
+    if (textdata) {
+      //var metadata = textdata.contents()[0].body.innerHTML;
+      var metadata = $('pre', textdata.contents()).text(); // TODO Chrome only?
+      $(metadata.split('\n')).each(function (i, x) {
+        var kv = x.split('=');
+        var k = kv[0];
+        var v = kv[1];
+        if (v) {
+          if (v.length == 0) {
+            localStorage.removeItem(k);
+          }
+          else {
+            localStorage.setItem(k, v);
+          }
+        }
+      });
+    }
+  });
 });
